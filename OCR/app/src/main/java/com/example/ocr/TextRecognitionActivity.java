e198018e8b31882c52f8d74388349e69c24d6172
ag502 <ag502@naver.com>
2020-01-13 17:34:41 +0900

    OCR TEST

---
+++ OCR/app/src/main/java/com/example/ocr/TextRecognitionActivity.java
@@ -0,0 +1,369 @@
+package com.example.ocr;
+
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Point;
+import android.graphics.Rect;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.ml.vision.FirebaseVision;
+import com.google.firebase.ml.vision.common.FirebaseVisionImage;
+import com.google.firebase.ml.vision.common.FirebaseVisionImageMetadata;
+import com.google.firebase.ml.vision.document.FirebaseVisionCloudDocumentRecognizerOptions;
+import com.google.firebase.ml.vision.document.FirebaseVisionDocumentText;
+import com.google.firebase.ml.vision.document.FirebaseVisionDocumentTextRecognizer;
+import com.google.firebase.ml.vision.text.FirebaseVisionCloudTextRecognizerOptions;
+import com.google.firebase.ml.vision.text.FirebaseVisionText;
+import com.google.firebase.ml.vision.text.FirebaseVisionTextRecognizer;
+import com.google.firebase.ml.vision.text.RecognizedLanguage;
+
+import org.opencv.android.OpenCVLoader;
+import org.opencv.android.Utils;
+import org.opencv.core.Mat;
+import org.opencv.imgproc.Imgproc;
+
+import java.io.File;
+import java.io.InputStream;
+import java.util.Arrays;
+import java.util.List;
+
+public class TextRecognitionActivity extends AppCompatActivity {
+
+    Button mButton;
+    Button textButton;
+    ImageView mImage;
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 0) {
+            if (resultCode == RESULT_OK) {
+                try {
+                    InputStream in = getContentResolver().openInputStream(data.getData());
+                    Bitmap img = BitmapFactory.decodeStream(in);
+                    in.close();
+                    VisionImage v = new VisionImage();
+                    recognizeText(v.imageFromBitmap(img));
+                    mImage.setImageBitmap(img);
+                } catch (Exception e) {
+
+                }
+            } else if (requestCode == RESULT_CANCELED) {
+                Toast.makeText(this, "사진 선택 취소", Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mButton = findViewById(R.id.my_button);
+        mImage = findViewById(R.id.image);
+        textButton = findViewById(R.id.text_button);
+
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_PICK);
+                startActivityForResult(intent, 0);
+            }
+        });
+
+//        try {
+//            InputStream is = getResources().getAssets().open("mycard1.jpg");
+//            Bitmap bitmap = BitmapFactory.decodeStream(is);
+//            VisionImage v = new VisionImage();
+//            recognizeText(v.imageFromBitmap(bitmap));
+//        } catch (Exception e) {
+//
+//        }
+
+//
+//        File file  = new File("app/src/main/assets/myCard.jpg");
+//        if (file.exists()) {
+//            Log.d("PRINT", "------------------------exist------------------------------");
+//            Bitmap myBitmap = BitmapFactory.decodeFile(file.getAbsolutePath());
+//
+//            Bitmap image1;
+//
+//            OpenCVLoader.initDebug();
+//
+//            Mat img1 = new Mat();
+//            Utils.bitmapToMat(myBitmap, img1);
+//            Mat imageGray1 = new Mat();
+//            Mat imageCny1 = new Mat();
+//
+//            Imgproc.threshold(imageGray1, imageCny1, 150, 255, Imgproc.THRESH_BINARY);
+//
+//            image1 = Bitmap.createBitmap(imageCny1.cols(), imageCny1.rows(), Bitmap.Config.ARGB_8888);
+//            Utils.matToBitmap(imageCny1, image1);
+//
+//            mImage.setImageBitmap(image1);
+//        } else {
+//            Log.d("PRINT", "------------------------doesn't exist------------------------------");
+//        }
+    }
+
+//    //My Version
+//    private void runTextRegocnition() {
+//        FirebaseVisionImage image = FirebaseVisionImage.fromBitmap(mImage.getDrawingCache());
+//        FirebaseVisionTextRecognizer detector = FirebaseVision.getInstance()
+//                .getOnDeviceTextRecognizer();
+//        mButton.setEnabled(false);
+//        detector.processImage(image)
+//                .addOnSuccessListener(
+//                        new OnSuccessListener<FirebaseVisionText>() {
+//                            @Override
+//                            public void onSuccess(FirebaseVisionText firebaseVisionText) {
+//                                textButton.setEnabled(false);
+//                                processTextRecognitionResult(firebaseVisionText);
+//                            }
+//                        })
+//                .addOnFailureListener(
+//                        new OnFailureListener() {
+//                            @Override
+//                            public void onFailure(@NonNull Exception e) {
+//                                textButton.setEnabled(true);
+//                                e.printStackTrace();
+//                            }
+//                        });
+//    }
+//
+//    private void processTextRecognitionResult(FirebaseVisionText texts) {
+//        List<FirebaseVisionText.TextBlock> blocks = texts.getTextBlocks();
+//        if (blocks.size() == 0) {
+//            Toast.makeText(getApplicationContext(), "No Text Found", Toast.LENGTH_SHORT).show();
+//            return;
+//        }
+//        for (int i = 0; i < blocks.size(); i++) {
+//            List<FirebaseVisionText.Line> lines = blocks.get(i).getLines();
+//            for (int j = 0; j < lines.size(); j++) {
+//                List<FirebaseVisionText.Element> elements = lines.get(j).getElements();
+//            }
+//        }
+//    }
+
+    private void recognizeText(FirebaseVisionImage image) {
+
+        // [START get_detector_default]
+        FirebaseVisionTextRecognizer detector = FirebaseVision.getInstance()
+                .getOnDeviceTextRecognizer();
+        // [END get_detector_default]
+
+        // [START run_detector]
+//        Task<FirebaseVisionText> result =
+        detector.processImage(image)
+                .addOnSuccessListener(new OnSuccessListener<FirebaseVisionText>() {
+                    @Override
+                    public void onSuccess(FirebaseVisionText firebaseVisionText) {
+                        // Task completed successfully
+                        // [START_EXCLUDE]
+                        // [START get_text]
+                        for (FirebaseVisionText.TextBlock block : firebaseVisionText.getTextBlocks()) {
+                            Rect boundingBox = block.getBoundingBox();
+                            Point[] cornerPoints = block.getCornerPoints();
+                            String text = block.getText();
+
+                            Log.d("PRINT", "-----------------------------------" + text);
+
+                            for (FirebaseVisionText.Line line: block.getLines()) {
+                                // ...
+                                for (FirebaseVisionText.Element element: line.getElements()) {
+                                    // ...
+
+                                }
+                            }
+                        }
+                        // [END get_text]
+                        // [END_EXCLUDE]
+                    }
+                })
+                .addOnFailureListener(
+                        new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                // Task failed with an exception
+                                // ...
+                                Log.d("PRINT", "-----------------------------------Failure------------------");
+                            }
+                        });
+        // [END run_detector]
+    }
+
+    private void recognizeTextCloud(FirebaseVisionImage image) {
+        // [START set_detector_options_cloud]
+        FirebaseVisionCloudTextRecognizerOptions options = new FirebaseVisionCloudTextRecognizerOptions.Builder()
+                .setLanguageHints(Arrays.asList("en", "hi"))
+                .build();
+        // [END set_detector_options_cloud]
+
+        // [START get_detector_cloud]
+        FirebaseVisionTextRecognizer detector = FirebaseVision.getInstance()
+                .getCloudTextRecognizer();
+        // Or, to change the default settings:
+        //   FirebaseVisionTextRecognizer detector = FirebaseVision.getInstance()
+        //          .getCloudTextRecognizer(options);
+        // [END get_detector_cloud]
+
+        // [START run_detector_cloud]
+        Task<FirebaseVisionText> result = detector.processImage(image)
+                .addOnSuccessListener(new OnSuccessListener<FirebaseVisionText>() {
+                    @Override
+                    public void onSuccess(FirebaseVisionText result) {
+                        // Task completed successfully
+                        // [START_EXCLUDE]
+                        // [START get_text_cloud]
+                        for (FirebaseVisionText.TextBlock block : result.getTextBlocks()) {
+                            Rect boundingBox = block.getBoundingBox();
+                            Point[] cornerPoints = block.getCornerPoints();
+                            String text = block.getText();
+
+                            for (FirebaseVisionText.Line line: block.getLines()) {
+                                // ...
+                                for (FirebaseVisionText.Element element: line.getElements()) {
+                                    // ...
+                                }
+                            }
+                        }
+                        // [END get_text_cloud]
+                        // [END_EXCLUDE]
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        // Task failed with an exception
+                        // ...
+                    }
+                });
+        // [END run_detector_cloud]
+    }
+
+    private void processTextBlock(FirebaseVisionText result) {
+        // [START mlkit_process_text_block]
+        String resultText = result.getText();
+        for (FirebaseVisionText.TextBlock block: result.getTextBlocks()) {
+            String blockText = block.getText();
+            Float blockConfidence = block.getConfidence();
+            List<RecognizedLanguage> blockLanguages = block.getRecognizedLanguages();
+            Point[] blockCornerPoints = block.getCornerPoints();
+            Rect blockFrame = block.getBoundingBox();
+            for (FirebaseVisionText.Line line: block.getLines()) {
+                String lineText = line.getText();
+                Float lineConfidence = line.getConfidence();
+                List<RecognizedLanguage> lineLanguages = line.getRecognizedLanguages();
+                Point[] lineCornerPoints = line.getCornerPoints();
+                Rect lineFrame = line.getBoundingBox();
+                for (FirebaseVisionText.Element element: line.getElements()) {
+                    String elementText = element.getText();
+                    Float elementConfidence = element.getConfidence();
+                    List<RecognizedLanguage> elementLanguages = element.getRecognizedLanguages();
+                    Point[] elementCornerPoints = element.getCornerPoints();
+                    Rect elementFrame = element.getBoundingBox();
+                }
+            }
+        }
+        // [END mlkit_process_text_block]
+    }
+
+    private FirebaseVisionDocumentTextRecognizer getLocalDocumentRecognizer() {
+        // [START mlkit_local_doc_recognizer]
+        FirebaseVisionDocumentTextRecognizer detector = FirebaseVision.getInstance()
+                .getCloudDocumentTextRecognizer();
+        // [END mlkit_local_doc_recognizer]
+
+        return detector;
+    }
+
+    private FirebaseVisionDocumentTextRecognizer getCloudDocumentRecognizer() {
+        // [START mlkit_cloud_doc_recognizer]
+        // Or, to provide language hints to assist with language detection:
+        // See https://cloud.google.com/vision/docs/languages for supported languages
+        FirebaseVisionCloudDocumentRecognizerOptions options =
+                new FirebaseVisionCloudDocumentRecognizerOptions.Builder()
+                        .setLanguageHints(Arrays.asList("en", "hi"))
+                        .build();
+        FirebaseVisionDocumentTextRecognizer detector = FirebaseVision.getInstance()
+                .getCloudDocumentTextRecognizer(options);
+        // [END mlkit_cloud_doc_recognizer]
+
+        return detector;
+    }
+
+    private void processDocumentImage() {
+        // Dummy variables
+        FirebaseVisionDocumentTextRecognizer detector = getLocalDocumentRecognizer();
+        FirebaseVisionImage myImage = FirebaseVisionImage.fromByteArray(new byte[]{},
+                new FirebaseVisionImageMetadata.Builder().build());
+
+        // [START mlkit_process_doc_image]
+        detector.processImage(myImage)
+                .addOnSuccessListener(new OnSuccessListener<FirebaseVisionDocumentText>() {
+                    @Override
+                    public void onSuccess(FirebaseVisionDocumentText result) {
+                        // Task completed successfully
+                        // ...
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        // Task failed with an exception
+                        // ...
+                    }
+                });
+        // [END mlkit_process_doc_image]
+    }
+
+    private void processDocumentTextBlock(FirebaseVisionDocumentText result) {
+        // [START mlkit_process_document_text_block]
+        String resultText = result.getText();
+        for (FirebaseVisionDocumentText.Block block: result.getBlocks()) {
+            String blockText = block.getText();
+            Float blockConfidence = block.getConfidence();
+            List<RecognizedLanguage> blockRecognizedLanguages = block.getRecognizedLanguages();
+            Rect blockFrame = block.getBoundingBox();
+            for (FirebaseVisionDocumentText.Paragraph paragraph: block.getParagraphs()) {
+                String paragraphText = paragraph.getText();
+                Float paragraphConfidence = paragraph.getConfidence();
+                List<RecognizedLanguage> paragraphRecognizedLanguages = paragraph.getRecognizedLanguages();
+                Rect paragraphFrame = paragraph.getBoundingBox();
+                for (FirebaseVisionDocumentText.Word word: paragraph.getWords()) {
+                    String wordText = word.getText();
+                    Float wordConfidence = word.getConfidence();
+                    List<RecognizedLanguage> wordRecognizedLanguages = word.getRecognizedLanguages();
+                    Rect wordFrame = word.getBoundingBox();
+                    for (FirebaseVisionDocumentText.Symbol symbol: word.getSymbols()) {
+                        String symbolText = symbol.getText();
+                        Float symbolConfidence = symbol.getConfidence();
+                        List<RecognizedLanguage> symbolRecognizedLanguages = symbol.getRecognizedLanguages();
+                        Rect symbolFrame = symbol.getBoundingBox();
+                    }
+                }
+            }
+        }
+        // [END mlkit_process_document_text_block]
+    }
+
+}
